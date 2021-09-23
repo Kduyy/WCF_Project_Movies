d033ec78da08799a09642feae130627fea73328c
Kduyy <88881428+Kduyy@users.noreply.github.com>
2021-09-23 20:50:07 +0700

    develop1

---
+++ WcfOnThi/Service1.svc.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfOnThi
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+        ExamEntities1 movieService = new ExamEntities1();
+        public List<Movie> getAll()
+        {
+
+            List<Movie> movie_list = new List<Movie>();
+            var list = from m in movieService.Movies select m;
+            foreach (var item in list)
+            {
+                Movie mve = new Movie();
+                mve.MovieId = item.MovieId;
+                mve.Title = item.Title;
+                mve.ReleaseDate = item.ReleaseDate;
+                mve.RunningTime = item.RunningTime;
+                mve.GenreId = item.GenreId;
+                mve.BoxOffice = item.BoxOffice;
+                movie_list.Add(mve);
+            }
+            return movie_list;
+        }
+
+        public List<Movie> Search(string Search)
+        {
+            List<Movie> movie_list = new List<Movie>();
+            var list = from m in movieService.Movies where m.Title.Contains(Search) select m;
+            foreach (var item in list)
+            {
+                Movie mve = new Movie();
+                mve.MovieId = item.MovieId;
+                mve.Title = item.Title;
+                mve.ReleaseDate = item.ReleaseDate;
+                mve.RunningTime = item.RunningTime;
+                mve.GenreId = item.GenreId;
+                mve.BoxOffice = item.BoxOffice;
+                movie_list.Add(mve);
+            }
+            return movie_list;
+        }
+    
+
+        public Movie GetbyId(int id)
+        {
+           var list = from m in movieService.Movies where m.MovieId == id select m;
+            List<Movie> movie_list = new List<Movie>();
+            Movie mve = null;
+            foreach (var item in list)
+            {
+                mve = new Movie();
+                mve.MovieId = item.MovieId;
+                mve.Title = item.Title;
+                mve.ReleaseDate = item.ReleaseDate;
+                mve.RunningTime = item.RunningTime;
+                mve.GenreId = item.GenreId;
+                mve.BoxOffice = item.BoxOffice;
+                movie_list.Add(mve);
+            }
+            return mve;
+        }
+
+        public void Add(Movie m)
+        {
+            Movie mve = new Movie();
+            mve.Title = m.Title;
+            mve.ReleaseDate = m.ReleaseDate;
+            mve.RunningTime = m.RunningTime;
+            mve.GenreId = m.GenreId;
+            mve.BoxOffice = m.BoxOffice;
+            movieService.Movies.Add(mve);
+
+            movieService.SaveChanges();
+        }
+
+        public void Edit(Movie m)
+        {
+            movieService.Entry(m).State = EntityState.Modified;
+            movieService.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            Movie mve = new Movie();
+            mve.MovieId = id;
+            movieService.Entry(mve).State = EntityState.Deleted;
+            try
+            {
+                movieService.SaveChanges();
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ WebApp_OnthiMVC/Controllers/MovieController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp_OnthiMVC.ServiceReference1;
+
+namespace WebApp_OnthiMVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        private Service1Client obj = new Service1Client();
+       
+        public ActionResult Index(String Search)
+        {
+            List<Movie> mve_list = new List<Movie>();
+            if (!(String.IsNullOrEmpty(Search)))
+            {
+                var list1 = obj.Search(Search);
+                foreach (var item in list1)
+                {
+                    Movie mve = new Movie();
+                    mve.MovieId = item.MovieId;
+                    mve.Title = item.Title;
+                    mve.ReleaseDate = item.ReleaseDate;
+                    mve.RunningTime = item.RunningTime;
+                    mve.GenreId = item.GenreId;
+                    mve.BoxOffice = item.BoxOffice;
+                    mve_list.Add(mve);
+                }
+                return View(mve_list);
+            }
+
+
+            var list = obj.getAll();
+
+            foreach (var item in list)
+            {
+                Movie mve = new Movie();
+                mve.MovieId = item.MovieId;
+                mve.Title = item.Title;
+                mve.ReleaseDate = item.ReleaseDate;
+                mve.RunningTime = item.RunningTime;
+                mve.GenreId = item.GenreId;
+                mve.BoxOffice = item.BoxOffice;
+                mve_list.Add(mve);
+            }
+            return View(mve_list);
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+
+            //ViewBag.lisMovie = obj.getAll();
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Movie m)
+        {
+
+            //ViewBag.lisMovie = obj.getAll();
+            obj.Add(m);
+            return RedirectToAction("Index", "Movie");
+        }
+        public ActionResult Edit(int id)
+        {
+            var item = obj.GetbyId(id);
+            Movie mve = new Movie();
+            mve.MovieId = item.MovieId;
+            mve.Title = item.Title;
+            mve.ReleaseDate = item.ReleaseDate;
+            mve.RunningTime = item.RunningTime;
+            mve.GenreId = item.GenreId;
+            mve.BoxOffice = item.BoxOffice;
+            //mve.Genre.GenreId = item.GenreId;
+            return View(mve);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Movie item)
+        {
+            if (ModelState.IsValid)
+            {
+
+                Movie ms = new Movie();
+
+                ms.MovieId = item.MovieId;
+                ms.Title = item.Title;
+                ms.ReleaseDate = item.ReleaseDate;
+                ms.RunningTime = item.RunningTime;
+                ms.GenreId = item.GenreId;
+                ms.BoxOffice = item.BoxOffice;
+                //ms.GenreId = item.Genre.GenreId;
+
+                int i = 0;
+                i++;
+                obj.Edit(ms);
+                return RedirectToAction("Index", "Movie");
+            }
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                obj.Delete(id);
+                return RedirectToAction("Index", "Movie");
+            }
+            return View();
+        }
+        public ActionResult Details(int id)
+        {
+            var list = obj.GetbyId(id);
+            Movie cls = new Movie();
+            cls.MovieId = list.MovieId;
+            cls.Title = list.Title;
+            cls.ReleaseDate = list.ReleaseDate;
+            cls.RunningTime = list.RunningTime;
+            cls.GenreId = list.GenreId;
+            cls.BoxOffice = list.BoxOffice;
+            return View(cls);
+        }
+    }
+}
+    
